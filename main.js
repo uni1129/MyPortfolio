131e2259717664f8c196a0ac6f99394c4abe783a
uni1129 <risajm.0307@icloud.com>
2025-07-28 11:50:09 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// ハンバーガーバー
 {
   const open = document.getElementById('open');
   const close = document.getElementById('close');
@@ -12,4 +15,47 @@
     overlay.classList.remove('show');
     open.classList.remove('hidden');
   });
+}
+
+// アコーディオン
+{
+
+}
+
+
+{
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('ol');
+  const slides = ul.children;
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  let currentIndex = 0;
+  function updateButtons() {
+    prev.classList.remove("hidden");
+    next.classList.remove("hidden");
+    if (currentIndex === 0) {
+      prev.classList.add("hidden");
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add("hidden");
+    }
+
+  }
+  function moveSlides() {
+    updateButtons();
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  }
+
+  const button = document.createElement("button");
+
+  updateButtons();
+  next.addEventListener('click', () => {
+    currentIndex++;
+    moveSlides();
+  });
+  prev.addEventListener('click', () => {
+    currentIndex--;
+    moveSlides();
+  });
+
 }
